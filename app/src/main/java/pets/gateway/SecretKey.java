6c69a7530d452bd4db81b5ce4cea39bfec53e92d
Bibek Aryal <34973169+bibekaryal86@users.noreply.github.com>
2022-01-18 17:47:42 -0700

    Update app.yaml (#6)

--- app/src/main/java/pets/gateway/SecretKey.java
+++ app/src/main/java/pets/gateway/SecretKey.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public class SecretKey_DUMMY {  // NOSONAR
+public class SecretKey {
 
     private static final String SECRET_KEY = "base_64_encoded_secret_key";
 
