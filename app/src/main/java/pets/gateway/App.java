42d5fb7ffdbd81526e0b72ed113c4f988f967f3f
Bibek Aryal <34973169+bibekaryal86@users.noreply.github.com>
2023-04-20 10:22:55 -0700

    Sleuth to Micrometer Updates (#8)

--- app/src/main/java/pets/gateway/App.java
+++ app/src/main/java/pets/gateway/App.java
@@ -5,11 +5,13 @@ package pets.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class App {
 
   public static void main(String[] args) {
+    Hooks.enableAutomaticContextPropagation();
     SpringApplication.run(App.class, args);
   }
 }
